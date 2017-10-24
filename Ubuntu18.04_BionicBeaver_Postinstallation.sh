2532f04285783ddf911720add4abd1145cea0f04
GammaDraconis <33062503+BionicBeaver@users.noreply.github.com>
2017-10-24 19:12:43 +0200

    Update Ubuntu18.04_BionicBeaver_Postinstallation.sh

--- Ubuntu18.04_BionicBeaver_Postinstallation.sh
+++ Ubuntu18.04_BionicBeaver_Postinstallation.sh
@@ -20,3 +20,20 @@
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
+
+
+# Contrôle de la configuration système (script correctement lancé + version 18.04 + gnome-shell présent)
+. /etc/lsb-release
+if [ "$UID" -ne "0" ]
+then
+    echo "Ce script doit se lancer avec les droits d'administrateur : sudo ./script.sh"
+    elif  [ "$DISTRIB_RELEASE" != "18.04" ]
+    then
+        echo "Désolé $SUDO_USER, ce script n'est conçu que pour la 18.04LTS alors que tu es actuellement sur la version $DISTRIB_RELEASE"
+        elif [ "$(which gnome-shell)" != "/usr/bin/gnome-shell" ]
+        then
+            echo "Bien que tu sois effectivement sur la 18.04 $SUDO_USER, ce script est conçu uniquement pour la version de base sous Gnome-Shell (pour l'instant) alors que tu utilises une variante..
+            exit
+            else
+                echo "Ok, tu as correctement lancé le script, tu es bien sur Bionic avec Gnome-Shell, passons aux questions..."
+fi
