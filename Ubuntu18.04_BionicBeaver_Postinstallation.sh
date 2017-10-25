656f5f865b960d145882151f80aeef5298d03c1f
GammaDraconis <33062503+BionicBeaver@users.noreply.github.com>
2017-10-25 09:37:21 +0200

    Update Ubuntu18.04_BionicBeaver_Postinstallation.sh

--- Ubuntu18.04_BionicBeaver_Postinstallation.sh
+++ Ubuntu18.04_BionicBeaver_Postinstallation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# version 0.0.2 (alpha)
+# version 0.0.3 (alpha)
 
 # Important : Ce script est en cours de développement, il n'est pas utilisable/testable pour l'instant !
 # Warning : This script is under development, it is not usable for the moment !
@@ -39,3 +39,38 @@ then
             else
                 echo "Ok, tu as correctement lancé le script, tu es bien sur Bionic avec Gnome-Shell, passons aux questions..."
 fi
+
+# Question 1 : sélection du mode de lancement du script
+echo "*******************************************************"
+echo "1/ Mode de lancement du script : "
+echo "*******************************************************"
+echo "[1] Mode standard (choix par défaut recommandé pour la plupart des utilisateurs)"
+echo "[2] Mode avancé (pour les utilisateurs plus experimentés)"
+echo "*******************************************************"
+read -p "Répondre par le chiffre correspondant (exemple : 1) : " choixMode
+clear
+
+while [ "$choixMode" != "1" ] && [ "$choixMode" != "2" ]
+do
+    read -p "Désolé, je ne comprend pas ta réponse, c'est soit 1 soit 2, merci de refaire ton choix : " choixMode
+    clear
+done
+
+echo "======================================================="
+echo "Précision : si vous validez directement par 'entrée' ou un choix non proposé, pour toutes les prochaines questions le script considèra que vous avez sélectionné le choix par défaut cad 1"
+# Question 2 : Session 
+echo "*******************************************************"
+echo "2/ Quelle(s) session(s) supplémentaire(s) souhaites-tu installer ? (plusieurs choix possibles)"
+echo "*******************************************************"
+echo "[1] Aucune, rester avec la session Ubuntu par défaut (cad Gnome customizé + 2 extensions)"
+echo "[2] Ajouter la session 'Gnome Vanilla' (cad une session Gnome non-customizé et sans extension)"
+echo "[3] Ajouter la session 'Gnome Classique' (interface plus traditionnelle dans le style de Gnome 2 ou Mate)"
+echo "[4] Ajouter la session 'Unity' (l'ancienne interface d'Ubuntu utilisé avant la 17.10"
+echo "*******************************************************"
+read -p "Répondre par le ou les chiffres correspondants séparés d'un espace (exemple : 2 3) : " choixSession
+clear
+
+
+
+    
+
